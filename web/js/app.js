92ce806d31cf2c08e39abd16f2120ba8fa9f318a
dimassimedwassim <dimassi.wassim@gmail.com>
2016-12-30 22:32:19 +0100

    added filter to flights

--- web/js/app.js
+++ web/js/app.js
@@ -3,14 +3,32 @@
  */
 var agency = angular.module('myApp',['ngRoute']);
 
+agency.controller('FlightsController',['$scope','$http',function ($scope,$http) {
+    $http.get('http://192.168.1.100:8080/RestOffreVol/webresources/flights').success(function (data) {
+        $scope.flights=data;
+
+
+    });
+}]);
+
+agency.controller('HomeController',['$scope',function ($scope) {
+    
+}]);
+
 agency.config(['$routeProvider',function($routeProvider) {
     $routeProvider
         .when("/home", {
-            templateUrl : "views/home.html"
+            templateUrl : "views/home.html",
+            controller: 'HomeController'
         })
-        .when("/blog", {
-        templateUrl : "views/blog.html"
+        .when("/flights", {
+        templateUrl : "views/flights.html",
+            controller: 'FlightsController'
     })
+        .when("/cars", {
+            templateUrl : "views/cars.html",
+            //controller: 'CarsController'
+        })
         .otherwise({
             redirectTo : "/home"
         });
